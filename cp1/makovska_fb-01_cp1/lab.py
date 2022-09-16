884dcf55f8f5b5284fc9a90d829dd1d9f492e823
makriin <99948135+makriin@users.noreply.github.com>
2022-09-16 18:53:25 +0300

    Update lab.py
    додала функцію для рахування частоти букв з пробілами

--- cp1/makovska_fb-01_cp1/lab.py
+++ cp1/makovska_fb-01_cp1/lab.py
@@ -16,3 +16,24 @@ def filter_txt( file_name, spaces = True ):
     new_file.write(new_txt)
     new_file.close()
 
+def frequency_of_letters(txt, alfavit):
+    dictionary = {} # створ словник, куди заносимо значення у відношенні літера:кількість зустріваності
+    for l in alfavit:
+        dictionary[l] = 0
+    for l in txt: # якщо літера є в тексті, збільшуємо її частоту зустріваності на 1
+        dictionary[l] += 1
+
+    freq = {} # рахуємо частоту для кожної літери і заносимо в словник
+    for l in alfavit:
+        freq[l] = (dictionary[l])/len(txt)
+        freq[l] = round(freq[l], 6)
+    return freq
+
+# готуємо текст до подальших обчислень
+filter_txt('text.txt', True)
+file = open('filtered.txt')
+text = file.read()
+file.close()
+
+alfavit_sp = 'абвгдеёэжзиыйклмнопрстуфхцчшщъьюя ' # алфавіт + пробіл
+print('Частота букв в тексті (з пробілами): \n\n', frequency_of_letters(text, alfavit_sp))
