9c13d799b75c7d1a39abef6d0d38f06c4023302c
makriin <99948135+makriin@users.noreply.github.com>
2022-09-17 16:27:54 +0300

    Update lab.py
    H1, H2

--- cp1/makovska_fb-01_cp1/lab.py
+++ cp1/makovska_fb-01_cp1/lab.py
@@ -1,5 +1,8 @@
 import re
 
+alfavit = 'абвгдеёэжзиыйклмнопрстуфхцчшщъьюя' # алфавіт
+alfavit_sp = 'абвгдеёэжзиыйклмнопрстуфхцчшщъьюя ' # алфавіт + пробіл
+
 def filter_txt( file_name, spaces = True ):
     # зчитуємо текст
     file = open( file_name, encoding="utf8" )
@@ -17,10 +20,10 @@ def filter_txt( file_name, spaces = True ):
     new_file.close()
 
 def frequency_of_letters(txt, alfavit):
-    dictionary = {} # створ словник, куди заносимо значення у відношенні літера:кількість зустріваності
+    dictionary = {} # словник зі значеннями літера:кількість зустріваності
     for l in alfavit:
         dictionary[l] = 0
-    for l in txt: # якщо літера є в тексті, збільшуємо її частоту зустріваності на 1
+    for l in txt: # якщо літера є в тексті, збільшуємо її частоту зустріваності
         dictionary[l] += 1
 
     freq = {} # рахуємо частоту для кожної літери і заносимо в словник
@@ -29,11 +32,58 @@ def frequency_of_letters(txt, alfavit):
         freq[l] = round(freq[l], 6)
     return freq
 
-# готуємо текст до подальших обчислень
+def frequency_of_bigrams(txt, alfavit, cross = True):
+    dictionary = {} # словник зі значеннями біграма:кількість зустріваності
+    freq = {}
+    for l1 in alfavit:
+        for l2 in alfavit:
+            key = l1 + l2
+            dictionary[key] = 0
+
+    #H1 - беремо перехресні пари абвгде - аб бв вг гд ге
+    if (cross == True): 
+        for i in range(len(txt)-1):  # до передостанньої літери 
+            key = txt[i] + txt[i+1]  # беремо 2 літери, які стоять поруч в тексті
+            dictionary[key] += 1     # збільш частоту зустріваності їх пари
+
+        for key in dictionary.keys():   # для кожної біграми (пари літер) рахуємо частоту 
+            freq[key] = dictionary[key]/(len(txt)-1)   
+            freq[key] = round(freq[key], 6)
+
+    #H2 - беремо кожні 2 елемента абвгде - аб вг де
+    else: 
+        if len(txt) % 2 == 1:
+            txt += "а"   # щоб кожній літері була пара
+        i = 0
+        for i in range(len(txt) - 1):
+            if i % 2 == 1:
+                continue
+            key = txt[i] + txt[i+1]
+            dictionary[key] += 1     # рахуємо частоту в тексті
+
+        for key in dictionary.keys():
+            freq[key] = dictionary[key]/(len(txt)/2)
+            freq[key] = round(freq[key], 6)
+
+    return freq
+
 filter_txt('text.txt', True)
 file = open('filtered.txt')
 text = file.read()
 file.close()
 
-alfavit_sp = 'абвгдеёэжзиыйклмнопрстуфхцчшщъьюя ' # алфавіт + пробіл
-print('Частота букв в тексті (з пробілами): \n\n', frequency_of_letters(text, alfavit_sp))
+print('----------------------Обрахунки для тексту з пробілами-----------------------\n\n')
+print('Частота букв:\n', frequency_of_letters(text, alfavit_sp))
+print('\nЧастота біграм H1:\n', frequency_of_bigrams(text, alfavit_sp, True))
+print('\nЧастота біграм H2:\n', frequency_of_bigrams(text, alfavit_sp, False))
+
+print('\n\n----------------------Обрахунки для тексту без пробілів-----------------------\n\n')
+
+filter_txt('text.txt', False)
+file = open('filtered.txt')
+text_no_spaces = file.read()
+file.close()
+
+print('Частота букв:\n', frequency_of_letters(text_no_spaces, alfavit))
+print('\nЧастота біграм H1:\n', frequency_of_bigrams(text_no_spaces, alfavit, True))
+print('\nЧастота біграм H2:\n', frequency_of_bigrams(text_no_spaces, alfavit, False))
