740829b855739b614b4e9658fcf4b64938cce4df
lingvvarm <lingvvarm@gmail.com>
2022-09-19 20:19:14 +0300

    added calc_h2 and updated sample text

--- cp1/andreiev_fb-06_cp1/main.py
+++ cp1/andreiev_fb-06_cp1/main.py
@@ -7,39 +7,22 @@ with open('sample_text.txt', 'r') as f:
 
 #фільтрація тексту, повертає текст з пробілами 
 def filter_text(text):
-    text = re.sub(r'[^\w ]', '', text).replace("  ", " ").lower()
+    text = re.sub(r'[^а-яА-Я ]', '', text).replace("  ", " ").lower()
     return text
 
+text = filter_text(text)
+text_no_spaces = text.replace(" ", "")
 
 #підрахунок символів
 def char_count(text):
     char_counter = {}
-    text = filter_text(text)
 
     for char in text:
         char_counter.setdefault(char, 0)
         char_counter[char] += 1
 
-    #char_counter.pop(' ', None)
     char_counter = dict(sorted(char_counter.items(), key=lambda x: x[1], reverse=True))
     return char_counter
-
-#підрахунок біграм, перехресна - type=slide, неперехресна - type=block
-def bigram_count(text, type):
-    bigram_counter = {}
-    text = filter_text(text)
-    n = 0
-    if type == 'block':
-        n = 2
-    elif type == 'slide':
-        n = 1
-    for i in range(0, len(text) - 1, n):
-        bigram = text[i] + text[i+1]
-        bigram_counter.setdefault(bigram, 0)
-        bigram_counter[bigram] += 1
-
-    bigram_counter = dict(sorted(bigram_counter.items(), key=lambda x: x[1], reverse=True))
-    return bigram_counter
 
 
 #рахуємо h1
@@ -56,8 +39,36 @@ def calc_h1(text):
 
     print(f"H1 = {round(h1, 3)}")
 
-#рахуємо h2 перехресно - type=slide, неперехресно - type=block
-def calc_h2(text, type):
-    pass
+
+slide_bigrmas = [text[i:i+2] for i in range(0, len(text), 1)]
+block_bigrams = [text[i:i+2] for i in range(0, len(text), 2)]
+slide_bigrmas_no_spaces = [text_no_spaces[i:i+2] for i in range(0, len(text_no_spaces), 1)]
+block_bigrams_no_spaces = [text_no_spaces[i:i+2] for i in range(0, len(text_no_spaces), 2)]
+
+#рахуємо h2
+def calc_h2(bigrams):
+    bigrams_length = len(bigrams)
+    unique_bigrams = set(bigrams)
+    probabilities = {}
+
+    for i in unique_bigrams:
+        probabilities[i] = ('{:.4f}'.format(bigrams.count(i)/bigrams_length))
+
+    h2 = 0
+    for i in probabilities.values():
+        if float(i) <= 0:
+            continue
+        h2 += -float(i) * math.log2(float(i))
+
+    h2 = round((h2 / 2), 3)
+    print(f"H2 = {h2}")
+
+    
 
 
+calc_h1(text)
+calc_h1(text_no_spaces)
+calc_h2(slide_bigrmas)
+calc_h2(block_bigrams)
+calc_h2(slide_bigrmas_no_spaces)
+calc_h2(block_bigrams_no_spaces)
