4cb30e2fba1a44c1e6f2cbf9aa623048ac9f0074
Vlad <kononets.vladislav.kpi@gmail.com>
2022-10-02 22:03:06 +0300

    Написав функцію для підрахунку зустрічаємості біграм: bigram_frequency.

--- cp1/kononets_fb-06_cp1/lab1.py
+++ cp1/kononets_fb-06_cp1/lab1.py
@@ -50,3 +50,35 @@ def frequency_of_letters(text, my_alphabet):    # рахуємо частоту 
         letters_frequency[letter] = round((letters_count[letter])/len(text), 9)  # округлюю до 9 знаків для точності
 
     return letters_frequency
+
+
+def bigram_frequency(text, my_alphabet, boolean_value):     # частота зустрічаємості біграм
+    bigram_count = {}
+    bigram_frequency_is = {}
+    for letter_1 in my_alphabet:
+        for letter_2 in my_alphabet:
+            dict_key = letter_1 + letter_2
+            bigram_count[dict_key] = 0
+
+    if boolean_value == 1:          # H1
+        i = 0
+        while i < len(text) - 1:            # довжина тексту - 1 , для H1
+            key = text[i] + text[i+1]       # скріплюю літери біграм
+            bigram_count[key] = bigram_count[key] + 1   # підрахунок зустрічаємості кожної біграми
+            i = i + 1
+
+        for key in bigram_count.keys():
+            bigram_frequency_is[key] = round(bigram_count[key]/(len(text)-1), 9)  # округлюю до 9 знаків для точності
+
+    else:                       # H2
+        if len(text) % 2 == 1:  # біграми з кроком 2
+            text += "о"
+        i = 0
+        while i < len(text) - 1:            # усе інше аналогічно до H1
+            key = text[i] + text[i+1]
+            bigram_count[key] = bigram_count[key] + 1
+            i = i + 2                       # окрім цієї частини, бо крок 2
+
+        for key in bigram_count.keys():
+            bigram_frequency_is[key] = round(bigram_count[key]/(len(text)/2), 9)
+    return bigram_frequency_is
