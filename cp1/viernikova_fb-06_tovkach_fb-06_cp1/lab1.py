76ed564847d5f89cd87f229d69bd77a6a460b14b
203lv <lilivernikova@gmail.com>
2022-10-19 21:31:44 +0200

    added calc functions

--- cp1/viernikova_fb-06_tovkach_fb-06_cp1/lab1.py
+++ cp1/viernikova_fb-06_tovkach_fb-06_cp1/lab1.py
@@ -1,3 +1,5 @@
+from collections import Counter
+from math import log2
 # Спочатку відкриємо наш текст
 # text_file = open('cp1/viernikova_fb-06_tovkach_fb-06_cp1/text/test.txt', 'r').read()
 text_file = open('cp1/viernikova_fb-06_tovkach_fb-06_cp1/text/1.txt', 'r').read()
@@ -19,3 +21,30 @@ ns_text_file = text_file.replace(' ', '')
 # запишемо очищенні тести у файли
 open('cp1/viernikova_fb-06_tovkach_fb-06_cp1/text/1_space.txt', 'w').write(text_file)
 open('cp1/viernikova_fb-06_tovkach_fb-06_cp1/text/1_no_space.txt', 'w').write(ns_text_file)
+
+mono_text = Counter(text_file)
+#print(mono_text) 
+
+def l_freq(dict):
+    l_sum = sum(dict.values()) 
+    for i in dict.keys():
+        dict[i] = dict[i] / l_sum 
+        
+def l_ent(dict):
+    for i in dict.keys():
+        dict[i] = -(dict[i] * log2(dict[i])) 
+
+def l_R(entropy):
+        r = 1 - entropy / log2(32)
+        return r
+
+l_freq(mono_text)
+#print(mono_text)
+l_ent(mono_text)
+#print(mono_text)
+
+all_entropy = sum(mono_text.values())
+#print(all_entropy)
+#print(l_R(all_entropy))
+
+
