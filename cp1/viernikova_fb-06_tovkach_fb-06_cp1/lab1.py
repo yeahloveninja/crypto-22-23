96eba058a568e1a3f99602dcf078e0a7892aed26
203lv <lilivernikova@gmail.com>
2022-10-20 08:52:32 +0200

    final beautified code

--- cp1/viernikova_fb-06_tovkach_fb-06_cp1/lab1.py
+++ cp1/viernikova_fb-06_tovkach_fb-06_cp1/lab1.py
@@ -1,9 +1,10 @@
+# Імпортуємо потрібні бібліотеки
 from collections import Counter
-from math import log2
 import pandas as pd
+from math import log2
+
 # Спочатку відкриємо наш текст
-# text_file = open('cp1/viernikova_fb-06_tovkach_fb-06_cp1/text/test.txt', 'r').read()
-text_file = open('cp1/viernikova_fb-06_tovkach_fb-06_cp1/text/1.txt', 'r').read()
+text_file = open('text/1.txt', 'r').read()
 text_file = text_file.lower().replace('ъ', 'ь').replace('ё', "е").replace('\n',' ')
 
 alphabet = 'абвгдежзийклмнопрстуфхцчшщыьэюя '
@@ -20,77 +21,73 @@ while "  " in text_file:
 ns_text_file = text_file.replace(' ', '')
 
 # запишемо очищенні тести у файли
-# open('cp1/viernikova_fb-06_tovkach_fb-06_cp1/text/1_space.txt', 'w').write(text_file)
-# open('cp1/viernikova_fb-06_tovkach_fb-06_cp1/text/1_no_space.txt', 'w').write(ns_text_file)
+open('text/1_space.txt', 'w').write(text_file)
+open('text/1_no_space.txt', 'w').write(ns_text_file)
 
-mono_text = Counter(text_file)
-mono_ns_text = Counter(ns_text_file)
-bigram = Counter([text_file[i:i + 2] for i in range(0, len(text_file) - 1)])
-bigram_ns = Counter([ns_text_file[i:i + 2] for i in range(0, len(text_file) - 1)])
+# робимо функцію де робимо все що необхідно в лабі
+def lab(text):
+    # тут використавши метод Counter отримаємо словник де ключі - наші букви, а значення - їх кількість
+    mono_text = Counter(text)
+    # невеличка перевірка наявності пробілів у наданому тексті
+    space = False
+    if mono_text[' '] > 0:
+        space = True
 
-# print(bigram) 
-mn_df = pd.DataFrame(list(mono_text.items()), columns=['Монограма', 'к-сть.'])
-mnn_df = pd.DataFrame(list(mono_ns_text.items()), columns=['Монограма', 'к-сть.'])
-bg_df = pd.DataFrame(list(bigram.items()), columns=['Біграма', 'к-сть.'])
-bgn_df = pd.DataFrame(list(bigram_ns.items()), columns=['Біграма', 'к-сть.'])
-
-
-def l_freq(dict):
-    l_sum = sum(dict.values()) 
-    for i in dict.keys():
-        dict[i] = dict[i] / l_sum 
+    # тут рахуємо частоту з якою зустрічаються наші моно- або біграми
+    def l_freq(dict):
+        l_sum = sum(dict.values()) # для цього рахуємо суму всіх значень
+        for i in dict.keys():
+            dict[i] = dict[i] / l_sum # а після перебираємо за кожним ключем значення і записуємо туди його ділення на суму всіх значень 
         
-def l_ent(dict):
-    for i in dict.keys():
-        dict[i] = -(dict[i] * log2(dict[i])) 
+    # тут рахуємо ентропію за формулою з методички
+    def l_ent(dict):
+        for i in dict.keys():
+            dict[i] = -(dict[i] * log2(dict[i])) # знову перезаписуємо за ключами їх ентропію
 
-def l_R(entropy, space = False):
-    if space == True:
-        r = 1 - entropy / log2(32)
-        return r
-    else:
-        r = 1 - entropy / log2(31)
-        return r
+    # тут рахуємо надлишок за формулою, тут нам і потрібна перевірка чи є пробіл у тексті, щоб дізнатись скільки букв в нашому алфавіті
+    def l_R(entropy):
+        if space == True:
+            r = 1 - entropy / log2(32)
+            return r
+        else:
+            r = 1 - entropy / log2(31)
+            return r
+    
+    # тут викликаємо функції і додаємо отримані дані у датафрейм
+    def add_df(dict, df):
+        l_freq(dict)
+        df['Частота'] = dict.values()
+        l_ent(dict)
+        df['Ентропія'] = dict.values()
+        if dict['а'] > 0: # невеличка перевірка на те монограма у нас чи ні
+            all_entropy = sum(dict.values())
+        else:
+            all_entropy = sum(dict.values())/2
+        df.at[0,'Загальна Ентропія'] = all_entropy
+        df.at[0,'Надлишковість'] = l_R(all_entropy)
 
-l_freq(bigram)
-bg_df['Частота'] = bigram.values()
-l_ent(bigram)
-bg_df['Ентропія'] = bigram.values()
+    # тут записуємо у csv наші дані
+    def write_df(df, name):
+        if space == True:
+            df.to_csv(f'{name}_space.csv', index=False)
+        else:
+            df.to_csv(f'{name}_no_space.csv', index=False)
+    
+    #створюємо датафрейми та словники з нашими значеннями буквами *значення для монограми отримуємо на початку функції 
+    mono_df = pd.DataFrame(list(mono_text.items()), columns=["Буква","к-сть"])
+    add_df(mono_text, mono_df)
+    write_df(mono_df, "monogram")
 
-bg_df.to_csv('cp1/viernikova_fb-06_tovkach_fb-06_cp1/bigram.csv', index=False)
-all_entropy = sum(bigram.values())/2
-print(f'Загальна ентропія: {all_entropy}')
-print(f'Надлишковість: {l_R(all_entropy)}')
+    bigram = Counter([text[i:i + 2] for i in range(0, len(text) - 1)])
+    bi_df = pd.DataFrame(list(bigram.items()), columns=["Буква","к-сть"])
+    add_df(bigram, bi_df)
+    write_df(bi_df, "bigram")
 
-l_freq(mono_text)
-mn_df['Частота'] = mono_text.values()
-l_ent(mono_text)
-mn_df['Ентропія'] = mono_text.values()
+    bigram_c = Counter([text[i:i + 2] for i in range(0, len(text) - 1, 2)])
+    bi_c_df = pd.DataFrame(list(bigram_c.items()), columns=["Буква","к-сть"])
+    add_df(bigram_c, bi_c_df)
+    write_df(bi_c_df, "crossed_bigram")
 
-mn_df.to_csv('cp1/viernikova_fb-06_tovkach_fb-06_cp1/mono.csv', index=False)
-all_entropy = sum(mono_text.values())
-print(f'Загальна ентропія: {all_entropy}')
-print(f'Надлишковість: {l_R(all_entropy)}')
-
-l_freq(mono_ns_text)
-mnn_df['Частота'] = mono_ns_text.values()
-l_ent(mono_ns_text)
-mnn_df['Ентропія'] = mono_ns_text.values()
-
-mnn_df.to_csv('cp1/viernikova_fb-06_tovkach_fb-06_cp1/mono_ns.csv', index=False)
-all_entropy = sum(mono_ns_text.values())
-print(f'Загальна ентропія: {all_entropy}')
-print(f'Надлишковість: {l_R(all_entropy)}')
-
-l_freq(bigram_ns)
-bgn_df['Частота'] = bigram_ns.values()
-l_ent(bigram_ns)
-bgn_df['Ентропія'] = bigram_ns.values()
-
-bgn_df.to_csv('cp1/viernikova_fb-06_tovkach_fb-06_cp1/bigram_ns.csv', index=False)
-all_entropy = sum(bigram_ns.values())/2
-print(f'Загальна ентропія: {all_entropy}')
-print(f'Надлишковість: {l_R(all_entropy)}')
-
-
-
+# робимо виклик наших функцій
+lab(text_file)
+lab(ns_text_file)
