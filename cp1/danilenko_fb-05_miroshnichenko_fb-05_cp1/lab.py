6308ba728cbb87657fe282ced7000e94ea5a557f
DanilenkoDanil <69428366+DanilenkoDanil@users.noreply.github.com>
2022-10-27 01:31:01 +0300

    Add files via upload

--- cp1/danilenko_fb-05_miroshnichenko_fb-05_cp1/lab.py
+++ cp1/danilenko_fb-05_miroshnichenko_fb-05_cp1/lab.py
@@ -11,50 +11,39 @@ def find_entropy(frequency: dict, n: int):
     return entropy
 
 
-def find_ngram_with_intersection(target_text: str):
-    ready_chars = []
+def find_ngram(target_text: str, bi: bool):
     total_count = 0
     ngram_dict = {}
-    new_text = ''
-    for ne_i in target_text[0::2]:
-        new_text += ne_i
-    find_results = re.findall(r'\w\w', new_text)
-    for ngram in find_results:
-        if ngram not in ready_chars:
-            count_ngram = re.findall(f'{ngram}', new_text)
-            ngram_dict[ngram] = len(count_ngram)
-            total_count += len(count_ngram)
-            ready_chars.append(ngram)
-    for ne_i in target_text[1::2]:
-        new_text += ne_i
-    find_results = re.findall(r'\w\w', new_text)
-    ready_chars = []
-    for ngram in find_results:
-        if ngram not in ready_chars:
-            if ngram not in ngram_dict:
-                count_ngram = re.findall(f'{ngram}', new_text)
-                ngram_dict[ngram] = len(count_ngram)
-                total_count += len(count_ngram)
-                ready_chars.append(ngram)
+
+    if bi:
+        check = 2
+    else:
+        check = 1
+
+    counter = 1
+    target_text = list(target_text)
+    for ne_i_5 in range(len(target_text))[1:]:
+        if counter % check == 0:
+            if bi:
+                current_gram = str(target_text[ne_i_5-1]) + str(target_text[ne_i_5])
             else:
-                count_ngram = re.findall(f'{ngram}', new_text)
-                ngram_dict[ngram] += len(count_ngram)
-                total_count += len(count_ngram)
-                ready_chars.append(ngram)
+                current_gram = str(target_text[ne_i_5])
+            if current_gram in ngram_dict:
+                ngram_dict[current_gram] += 1
+                total_count += 1
+            else:
+                ngram_dict[current_gram] = 1
+                total_count += 1
+        counter += 1
     for ngram in ngram_dict:
         ngram_dict[ngram] = round(ngram_dict[ngram]/total_count, 6)
 
     return ngram_dict
 
 
-def find_ngram(target_text: str, bi: bool):
+def find_ngram_with_intersection(target_text: str):
     ready_chars = []
-
-    if bi is False:
-        n = 1
-    else:
-        n = 2
-    find_results = re.findall(r'\w' * n, target_text)
+    find_results = re.findall(r'\w\w', target_text)
     total_count = 0
     ngram_dict = {}
     for ngram in find_results:
@@ -73,41 +62,45 @@ def r_calculate(h, count):
     ans = 1 - (h/math.log2(count))
     return ans
 
+monogram = 1
+bigram = 2
+
+alpha_with_space = 34
+alpha_without_space = 33
 
 text_with_space = open("2.txt", 'r').read()
 text_without_space = open("3.txt", 'r').read()
 
-monogram = 1
-bigram = 2
-alpha_with_space = 34
-alpha_without_space = 33
+mono_gram_with_space = find_ngram(text_with_space, False)
+mono_gram_without_space = find_ngram(text_without_space, False)
+mono_gram_with_space_entropy = find_entropy(mono_gram_with_space, monogram)
+mono_gram_without_space_entropy = find_entropy(mono_gram_without_space, monogram)
+print(f"mono_gram_with_space_entropy - {mono_gram_with_space_entropy}")
+print(f"R mono_gram_with_space - {r_calculate(mono_gram_with_space_entropy, alpha_with_space)}")
+print(f"mono_gram_without_space_entropy - {mono_gram_without_space_entropy}")
+print(f"R mono_gram_without_space - {r_calculate(mono_gram_without_space_entropy, alpha_without_space)}")
 
 
-monogram_with_space = find_ngram(text_with_space, False)
-monogram_with_space_entropy = find_entropy(monogram_with_space, monogram)
-monogram_without_space = find_ngram(text_without_space, False)
-monogram_without_space_entropy = find_entropy(monogram_without_space, monogram)
-print(f'h1 with space - {monogram_with_space_entropy}')
-print(f'r1 with space - {r_calculate(monogram_with_space_entropy, alpha_with_space)}')
-print(f'h1 without space - {monogram_without_space_entropy}')
-print(f'r1 without space - {r_calculate(monogram_with_space_entropy, alpha_without_space)}')
+bi_gram_with_space = find_ngram(text_with_space, True)
+bi_gram_without_space = find_ngram(text_without_space, True)
+bi_gram_with_space_entropy = find_entropy(bi_gram_with_space, bigram)
+bi_gram_without_space_entropy = find_entropy(bi_gram_without_space, bigram)
+print(f"bi_gram_with_space_entropy - {bi_gram_with_space_entropy}")
+print(f"R bi_gram_with_space - {r_calculate(bi_gram_with_space_entropy, alpha_with_space)}")
+print(f"bi_gram_without_space_entropy - {bi_gram_without_space_entropy}")
+print(f"R bi_gram_without_space - {r_calculate(bi_gram_without_space_entropy, alpha_without_space)}")
 
+bi_gram_with_space_with_intersection = find_ngram_with_intersection(text_with_space)
+bi_gram_without_space_with_intersection = find_ngram_with_intersection(text_without_space)
+bi_gram_with_space_with_intersection_entropy = find_entropy(bi_gram_with_space_with_intersection, bigram)
+bi_gram_without_with_intersection_space_entropy = find_entropy(bi_gram_without_space_with_intersection, bigram)
+print(f"bi_gram_with_space_entropy_with_intersection - {bi_gram_with_space_with_intersection_entropy}")
+print(f"R bi_gram_with_space_with_intersection - {r_calculate(bi_gram_with_space_with_intersection_entropy, alpha_with_space)}")
+print(f"bi_gram_without_space_entropy_with_intersection - {bi_gram_without_with_intersection_space_entropy}")
+print(f"R bi_gram_without_space_with_intersection - {r_calculate(bi_gram_without_with_intersection_space_entropy, alpha_without_space)}")
 
-bigram_with_space = find_ngram_with_intersection(text_with_space)
-bigram_with_space_without_intersection = find_ngram(text_with_space, True)
-bigram_with_space_without_intersection_entropy = find_entropy(bigram_with_space_without_intersection, bigram)
-bigram_with_space_entropy = find_entropy(bigram_with_space, bigram)
-bigram_without_space = find_ngram_with_intersection(text_without_space)
-bigram_without_space_without_intersection = find_ngram(text_without_space, True)
-bigram_without_space_entropy = find_entropy(bigram_without_space, bigram)
-bigram_without_space_without_intersection_entropy = find_entropy(bigram_without_space_without_intersection, bigram)
-
-
-print(f'h2 with space - {bigram_with_space_entropy}')
-print(f'r2 with space - {r_calculate(bigram_with_space_entropy, alpha_with_space)}')
-print(f'h2 with space without intersection - {bigram_with_space_without_intersection_entropy}')
-print(f'r2 with space without intersection - {r_calculate(bigram_with_space_without_intersection_entropy, alpha_with_space)}')
-print(f'h2 without space - {bigram_without_space_entropy}')
-print(f'h2 without space without intersection - {bigram_without_space_without_intersection_entropy}')
-print(f'r2 without space without intersection - {r_calculate(bigram_without_space_without_intersection_entropy, alpha_without_space)}')
-print(f'r2 without space - {r_calculate(bigram_without_space_entropy, alpha_without_space)}')
+# for i in list(bi_gram_with_space_with_intersection.keys())[:15]:
+#     print(f"{i} - {bi_gram_with_space_with_intersection[i]}")
+# print('--------------------------')
+# for i in list(bi_gram_without_space_with_intersection.keys())[:15]:
+#     print(f"{i} - {bi_gram_without_space_with_intersection[i]}")
