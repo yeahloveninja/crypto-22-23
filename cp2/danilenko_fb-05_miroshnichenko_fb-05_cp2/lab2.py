fda4ff165ce30d979b71a5b9311bfc402253bdb6
Danil <danilenkodanil74@gmail.com>
2022-11-10 08:29:35 +0200

    mini fix 2

--- cp2/danilenko_fb-05_miroshnichenko_fb-05_cp2/lab2.py
+++ cp2/danilenko_fb-05_miroshnichenko_fb-05_cp2/lab2.py
@@ -21,8 +21,8 @@ def vigenere(target_text: str, code: str, type_operation: bool) -> str:
 def coincidence_index(target_text: str) -> float:
     length = len(target_text)
     index = 0
-    for i in range(len(alpha)):
-        count_letter = target_text.count(alpha[i])
+    for element in range(len(alpha)):
+        count_letter = target_text.count(alpha[element])
         index += count_letter * (count_letter - 1)
     index *= 1 / (length * (length - 1))
     return index
@@ -39,8 +39,8 @@ def indexes_blocks(target_text: str, length: int) -> int:
     block = split_blocks(target_text, length)
     start_index = 0
 
-    for i in range(len(block)):
-        start_index = start_index + coincidence_index(block[i])
+    for element in range(len(block)):
+        start_index = start_index + coincidence_index(block[element])
     start_index = start_index/len(block)
 
     return start_index
