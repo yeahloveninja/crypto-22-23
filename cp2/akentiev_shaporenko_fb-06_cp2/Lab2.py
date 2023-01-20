d379ae551d0975cb846fc2244191f317efe4667c
Akentiev-Vlad <akentiev.vladislav.kpi@gmail.com>
2023-01-20 13:43:10 +0200

    Lab2 sources added.

--- cp2/akentiev_shaporenko_fb-06_cp2/Lab2.py
+++ cp2/akentiev_shaporenko_fb-06_cp2/Lab2.py
@@ -3,6 +3,8 @@ import re
 alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
 file = open('text.txt', encoding='utf-8')
 text = file.read()
+file3 = open('3task.txt', encoding='utf-8').read()
+keys = open('keys.txt', encoding='utf-8', mode='r').read().split(' ')
 
 
 def fil_text(text):
@@ -11,6 +13,12 @@ def fil_text(text):
     return space
 
 
+task3 = fil_text(file3)
+thirdTask = task3.replace(' ', '')
+var6task3 = open('3taskvar6.txt', 'w', encoding='utf-8')
+var6task3.write(thirdTask)
+var6task3.close()
+
 textWithSpace = fil_text(text)
 textWithoutSpace = textWithSpace.replace(' ', '')
 
@@ -18,46 +26,51 @@ file_No_Space = open('filtText.txt', 'w', encoding='utf-8')
 file_No_Space.write(textWithoutSpace)
 file_No_Space.close()
 
-letter_to_index = dict(zip(alphabet, range(len(alphabet))))
-index_to_letter = dict(zip(range(len(alphabet)), alphabet))
+
+def encrypt(textWithoutSpace):
+    encrypted_texts = {i: ''.join([alphabet[(alphabet.find(textWithoutSpace[j]) + (alphabet.find(i[j % len(i)]))) % 32] for j in range(len(textWithoutSpace))]) for i in keys}
+    return encrypted_texts
 
 
-def encrypt(message, key):
-    encrypted = ""
-    split_message = [message[i : i + len(key)] for i in range(0, len(message), len(key))]
-    for each_split in split_message:
-        i = 0
-        for letter in each_split:
-            number = (letter_to_index[letter] + letter_to_index[key[i]]) % len(alphabet)
-            encrypted += index_to_letter[number]
-            i += 1
-
-    return encrypted
+def decrypt(thirdTask):
+    key = "возвращениеджинна"
+    result = ''.join([alphabet[(alphabet.find(thirdTask[i]) - (alphabet.find(key[i % len(key)]))) % 32] for i in range(len(thirdTask))])
+    return result
 
 
-def decrypt(cipher, key):
-    decrypted = ""
-    split_encrypted = [cipher[i: i + len(key)] for i in range(0, len(cipher), len(key))]
-    for each_split in split_encrypted:
-        i = 0
-        for letter in each_split:
-            number = (letter_to_index[letter] - letter_to_index[key[i]]) % len(alphabet)
-            decrypted += index_to_letter[number]
-            i += 1
+def index(thirdTask):
+    res = sum([thirdTask.count(i) * (thirdTask.count(i) - 1) for i in alphabet]) / (len(thirdTask) * (len(thirdTask) - 1))
+    return res
 
-    return decrypted
+
+def keyLength(thirdTask):
+    ind = {lengthKey: sum([index(thirdTask[group::lengthKey]) for group in range(lengthKey)])/lengthKey for lengthKey in range(2, len(alphabet))}
+    return ind
+
+
+def blocks(thirdTask):
+    blocks = [thirdTask[block::17] for block in range(17)]
+    return blocks
+
+
+def frequencies(thirdTask):
+    frequencies = [max({letter: block.count(letter) / len(block) for letter in set(block)}, key={letter: block.count(letter) / len(block) for letter in set(block)}.get) for block in blocks(thirdTask)]
+    return frequencies
+
+
+def findKey(thirdTask):
+    key = ''.join([alphabet[(alphabet.index(letter) - 14) % len(alphabet)] for letter in frequencies(thirdTask)])
+    return key
 
 
 def main():
-    message = input()
-    key = input()
-    encrypted_message = encrypt(message, key)
-    decrypted_message = decrypt(encrypted_message, key)
-
-    print("Текст: " + message)
-    print("Зашифрований текст: " + encrypted_message)
-    print("Розшифрований текст: " + decrypted_message)
+    encryptedtext = encrypt(textWithoutSpace)
+    print('Зашифрований текст:\n', encryptedtext)
+    print('Індекси:\n', keyLength(thirdTask))
+    print('Ключ:\n', findKey(thirdTask))
+    print('Розшифрований текст:\n', decrypt(thirdTask))
 
 
 main()
+
 
